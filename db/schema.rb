baed8c6f7113a36702111e059f0e27fe253b2171
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 19:36:43 +0200

    FIX: Lint & RuboCop corrections

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_23_141016) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
 end
